ae54fbc763f23415d0db26f8a7d93596d321cb4c
Dexter Wong <dexterwong0724@gmail.com>
2024-10-31 22:30:29 +0800

    amended config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,4 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/log-in-FSDP/', // Ensure the base path has trailing slashes and matches the repo name exactly
 });
