9cf9b274844a45b759efbd1cdd5a58e1099b0968
Dexter Wong <dexterwong0724@gmail.com>
2024-11-01 14:23:16 +0800

    ammended

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
